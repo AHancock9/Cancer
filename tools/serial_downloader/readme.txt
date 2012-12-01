5b2dce6fcb004ab7a0db45d27ce8efa5fdaea540
Insswer <qijiasi001@gmail.com>
2012-12-01 20:16:07 +0800

    check the downloader for uart boot and it can work fine now

--- tools/serial_downloader/readme.txt
+++ tools/serial_downloader/readme.txt
@@ -1,4 +1,2 @@
-NOTICE->>this tool has not been tested! 
-
 This tool is used to download a image into Omap4460's Internal RAM by UART. The default address is 0x40300000.
-
+Your code should link at 0x40300000.
--- tools/serial_downloader/serial_downloader.py
+++ tools/serial_downloader/serial_downloader.py
@@ -6,38 +6,43 @@
 
 import serial
 import struct
+import sys
 
 if len(sys.argv) != 3:
        print "usage: python serial_downloader <comnum> <filename>"
 com_num = sys.argv[1]
 file_name = sys.argv[2]
-mlo_img = open(filename, "rb")
+mlo_img = open(file_name, "rb")
 img = ""
 while True:
     img_temp = mlo_img.read(256)
     img = img + img_temp
     if len(img_temp) == 0:
         break
-print "read img done, size is "+str(img)+" bytes"
+print "read img done, size is "+str(len(img))+" bytes"
 mlo_img.close()
 img_size = len(img)
 ser = serial.Serial(com_num,115200)
 
 #asic id size is 81bytes
 while True:
+       print "check device..."
        asic_id = ser.read(81)
-       items,id_subblock,no_use0,no_use1,no_use2,crc = struct.unpack("=1B7B4B23B35B11B",asic_id)
-       print "read items: "+hex(items)
-       id_block = struct.unpack("=BBBSBBB",id_subblock)
-       if hex(id_block[3]) == "0x44" or hex(id_block[3]) =="0x40":
+       print "read 81 bytes"
+       result = struct.unpack("<81B",asic_id)
+       items = result[0:8]
+       print "read items: " + hex(items[0])
+       print "read dev : " + hex(items[4])
+       if hex(items[4]) == "0x44" or hex(items[4]) =="0x45":
               print "found Omap4460"
               print "check all right..."
-              print "ROM revision: "+hex(id_block[5])
+              print "ROM revision: "+hex(items[6])
 #continue booting with UART
               ser.write(0xf0030002)
               break
        else:
               print "wrong asic id format... try again"
+              ser.flushInput()
               ser.write(0xf0030003)
               continue
 
@@ -46,7 +51,13 @@ ser.write(img_size)
 print "transfer img.... "
 ser.write(img)
 print "transfer done..."
+
+print "try to interrupt auto boot"
+ser.write("\n")
+ser.write("\n")
 ser.close()
+              
+
               
 
 
